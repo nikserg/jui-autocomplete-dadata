67d555a8174ac60aa26724b5172775e741884c27
n.zarubin <n.zarubin@itsirius.ru>
2019-06-19 17:58:07 +0300

    init

---
+++ DadataAutoComplete.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: n.zarubin
+ * Date: 19.06.2019
+ * Time: 17:44
+ */
+
+namespace yii\jui;
+
+
+use yii\web\JsExpression;
+
+class DadataAutoComplete extends AutoComplete
+{
+    public $dadataAuthKey;
+    public $dadataUrl;
+    /**
+     * Дополнительные параметры, передаваемые в dadata.
+     *
+     * Например,
+     * [
+     * 'parts' => ['SURNAME']
+     * ]
+     *
+     * @var array
+     */
+    public $additionalParams;
+
+    protected function getDadataUrl() {
+        return $this->dadataUrl;
+    }
+    protected function getDadataAuthKey() {
+        if ($this->dadataAuthKey) {
+            return $this->dadataAuthKey;
+        }
+        else if (isset(\Yii::$app->params['dadataAuthKey']) && \Yii::$app->params['dadataAuthKey']) {
+            return \Yii::$app->params['dadataAuthKey'];
+        } else {
+            throw new \Exception('Dadata auth key not found. Either pass dadataAuthKey parameter to widget, or define a dadataAuthKey param in your app (in params.php)');
+        }
+    }
+    public function init()
+    {
+        parent::init(); 
+        if ($this->additionalParams) {
+            $additionalParamsJson = json_encode($this->additionalParams);
+        }
+        else {
+            $additionalParamsJson = '{}';
+        }
+        $this->clientOptions['source'] = new JsExpression("function(request,response){
+                            let data = $additionalParamsJson;
+                            data['query'] = request.term;
+                            $.ajax({
+                                type: 'POST',
+                                headers: {
+                                    'Authorization': 'Token ".$this->getDadataAuthKey()."'
+                                },
+                                url: '".$this->getDadataUrl()."',
+                                contentType: 'application/json',
+                                dataType: 'json',
+                                data: JSON.stringify(data),
+                                success: function (data) {
+                                    let result = [];
+                                    for (let i in data.suggestions) {
+                                        let row = data.suggestions[i];
+                                        result.push(row.unrestricted_value);
+                                    }
+                                    response(result);
+                                }
+                            });
+                        }");
+    }
+
+}
